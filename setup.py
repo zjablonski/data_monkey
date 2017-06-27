756bd1fce8e91f1033c8be04a9e06abe04e9016a
Zack J <zackj@scdg.io>
2017-06-27 12:50:28 -0600

    Basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data_monkey',
+      version='0.1',
+      description='convert files with preconfigured options',
+      url='http://github.com/zjablonski/data_monkey',
+      author='zjablonski',
+      author_email='zackj@scdg.io',
+      license='MIT',
+      packages=['data_monkey'],
+      zip_safe=False)
